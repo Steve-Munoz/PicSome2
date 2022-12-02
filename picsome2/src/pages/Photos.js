d9ee1d6149a0b6c7f9a55b480ef56e6cf128e92f
Steve Munoz <stevomunoz@yahoo.com>
2022-12-01 23:29:56 -0600

    created context and mapped over photos data

---
+++ picsome2/src/pages/Photos.js
@@ -0,0 +1,14 @@
+import React from "react"
+// import {Context} from "../Context"
+
+function Photos() {
+    // const {photos} = useContext()
+    return (
+        <main className="photos">
+            <h1>Images Go Here</h1>
+            {/* <h1>{photos}</h1> */}
+        </main>
+    )
+}
+
+export default Photos
