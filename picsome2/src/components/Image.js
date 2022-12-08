f0ce4520a4fc74394a5790d06ade76f10c474097
Steve Munoz <stevomunoz@yahoo.com>
2022-12-07 18:30:36 -0600

    create Image component to show heart and cart icon when hovered

--- picsome2/src/components/Image.js
+++ picsome2/src/components/Image.js
@@ -1,20 +1,26 @@
-import React from "react"
+import React,{useState} from "react"
 
 function Image({className, img}){
+    const [isHovered, setIsHovered] = useState(false)
+    const heartIcon = isHovered && <i className="ri-heart-line favorite"></i>
+    const cartIcon = isHovered && <i className="ri-add-circle-line cart"></i>
+    console.log(isHovered)
+
+ 
     return(
-        <div className = {`${className} image-container`}>
-            <img src ={img.url} className = "image-grid"/>
+        <div className = {`${className} image-container `}
+        onMouseEnter= {()=>setIsHovered(true) } 
+        onMouseLeave = {()=>setIsHovered(false) }
+       
+        
+        >
+            
+            <img src ={img.url} className = "image-grid " />
+            {heartIcon}
+            {cartIcon}
         </div>
     )
 }
 
 export default Image
-// function Image({className, img}) {
-//     return (
-//         <div className={`${className} image-container`}>
-//             <img src={img.url} className="image-grid"/>
-//         </div>
-//     )
-// }
 
-// export default Image
