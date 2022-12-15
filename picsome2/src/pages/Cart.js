82df316d9252a693c17c2650bdc11590b8e71ac4
Steve Munoz <stevomunoz@yahoo.com>
2022-12-15 17:27:44 -0600

    place order functionality

--- picsome2/src/pages/Cart.js
+++ picsome2/src/pages/Cart.js
@@ -1,13 +1,25 @@
-import React, {useContext} from "react"
+import React, {useContext, useState} from "react"
 import {Context} from "../Context"
 import CartItem from "../components/CartItem"
 
 
 function Cart() {
-    const {cartItems} = useContext(Context)
+    const {cartItems,emptyCart} = useContext(Context)
+    const [buttonText, setButtonText] = useState("Place Order")
     const cartItemElements = cartItems.map(item=><CartItem key ={item.id} item = {item}/>)
     const totalCost = 5.99 * cartItems.length
     const totalCostDisplay = totalCost.toLocaleString("en-US", {style: "currency", currency: "USD"})
+
+    function placeOrder(){
+        
+        setButtonText("Ordering..")
+        setTimeout(()=>{
+            emptyCart()
+            setButtonText("Order Placed")
+
+        },3000)
+        
+    }
     
     return (
         <main className="cart-page">
@@ -17,7 +29,7 @@ function Cart() {
             {cartItemElements}
             <p className="total-cost">Total:{totalCostDisplay} </p>
             <div className="order-button">
-                <button>Place Order</button>
+                <button onClick = {()=>placeOrder()}>{buttonText}</button>
             </div>
         </main>
     )
