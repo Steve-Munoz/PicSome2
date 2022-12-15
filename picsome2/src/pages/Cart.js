3e12a4e627c9c263d47657aab5b7f636f27388af
Steve Munoz <stevomunoz@yahoo.com>
2022-12-15 15:55:25 -0600

    cart page setup

--- picsome2/src/pages/Cart.js
+++ picsome2/src/pages/Cart.js
@@ -1,15 +1,22 @@
 import React, {useContext} from "react"
 import {Context} from "../Context"
+import CartItem from "../components/CartItem"
 
 
 function Cart() {
     const {cartItems} = useContext(Context)
-    console.log(cartItems)
+    const cartItemElements = cartItems.map(item=><CartItem key ={item.id} item = {item}/>)
+    
     return (
         <main className="cart-page">
-            {cartItems}
+            
             
             <h1>Check out</h1>
+            {cartItemElements}
+            <p className="total-cost">Total: </p>
+            <div className="order-button">
+                <button>Place Order</button>
+            </div>
         </main>
     )
 }
